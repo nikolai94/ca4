f9251b7765dd90e48266b9205080b0ec23d14358
Nikolai94 <nikolai@cybermanager.dk>
2015-04-28 12:08:02 +0200

    Facade & rest

--- server/model/facade.js
+++ server/model/facade.js
@@ -12,7 +12,17 @@ function getWiki(title,callback){
 
 
 function findWiki(title,callback){
-    wiki.find({title: new RegExp(title, 'i')}, function (err, data) {
+    wiki.find({title: new RegExp(title, 'i')},'title abstract', function (err, data) {
+        if (err) {
+            return callback(err);
+        }
+
+        return callback(null,data);
+    });
+}
+
+function getCategories(callback){
+    wiki.distinct('categories', function (err, data) {
         if (err) {
             return callback(err);
         }
@@ -20,18 +30,9 @@ function findWiki(title,callback){
     });
 }
 
-function getCategories(callback){
-    wiki.distinct('title', function (err, data) {
-        if (err) {
-            return callback(err);
-        }
-        return callback(data);
-    });
-}
-
 
 function getWikisWithCategory(category,callback){
-    wiki.find({categories: category}, function (err, data) {
+    wiki.find({categories: category},'title abstract', function (err, data) {
         if (err) {
             return callback(err);
         }
