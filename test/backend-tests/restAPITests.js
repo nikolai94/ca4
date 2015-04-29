f1850cd7cc13f0fbe9ae01045407fda366467f75
jonathanec94 <jona_egebak@hotmail.dk>
2015-04-29 09:40:27 +0200

    mocha test start

--- test/backend-tests/restAPITests.js
+++ test/backend-tests/restAPITests.js
@@ -4,8 +4,6 @@ require("../../server/model/db");
 var should = require("should");
 var app = require("../../server/app");
 var http = require("http");
-var testPort = 9999;
-var testServer;
 var facade = require('../../server/model/facade');
 var mongoose = require("mongoose");
 var Wiki = mongoose.model("wiki");
@@ -13,18 +11,23 @@ var Wiki = mongoose.model("wiki");
 
 
 var wikiOpret = [
-    {title: "TestNavn", url: "http://",abstract :"abstracs1",categories : "programming",links:"http://www.s.com", headings:"something"},
-    {title: "TestNavn2", url: "http://",abstract :"abstract2",categories : "football",links:"http://www.f.com", headings:"something else"}
+    {title: "TestNavn", url: "http://",abstract :"abstracs1",categories : ["programming"],links:["http://www.s.com"], headings:["something"]},
+    {title: "TestNavn2", url: "http://",abstract :"abstract2",categories : ["football"],links:["http://www.f.com"], headings:["something else"]}
 ];
 
 describe ('Wiki', function(){
 
     beforeEach(function (done){
         Wiki.remove({}, function () {
-            Wiki.create(wikiOpret, function (err, wikie) {
-                done();
+            Wiki.create(wikiOpret, function (err, newQuotes) {
+                if(err)
+                {
+                    console.log(err);
+                }
+
             })
         });
+        done();
     });
     after(function (done) {
         //Uncomment the lines below to completely remove the test database after the tests
@@ -34,20 +37,15 @@ describe ('Wiki', function(){
         done();
     });
 
+    it("return en title", function (done) {
+        facade.findWiki("Abraham Lincon",function (err, wikie) {
+            if (err) throw err;;
+            wikie.title.should.equal("Abraham Lincon");
 
-
-
-        it("return en title", function (done) {
-            facade.findWiki("Abraham Lincon",function (err, wikie) {
-                if (err) throw err;;
-                wikie.title.should.equal("Abraham Lincon");
-                done();
-            })
-        });
-
-
-
-
+        })
+        done();
+    });
+    
 });
 
 //getWiki
